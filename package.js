e887dad07b2b12760565f286517b7002a8ed25b7
Kristoffer Klintberg <krstffr@gmail.com>
2014-10-30 10:58:31 +0100

    Updated to new meteor package format.

--- package.js
+++ package.js
@@ -1,17 +1,16 @@
 Package.describe({
-  "summary": "A super simple slideshow thing."
+	name: "krstffr:slidr",
+  summary: "A super simple slideshow thing.",
+	version: "0.0.5",
+	git: "https://github.com/krstffr/meteor-slidr.git"
 });
 
-Package.on_use(function (api) {
+Package.onUse(function (api) {
 
 	api.add_files('lib/jqyeryFns.js', 'client');
   api.add_files('lib/slidr.js', 'client');
 
-  if (typeof api.export !== 'undefined') {
-
-    // The main object.
-    api.export('slidr', 'client');
-
-  }
+  // The main object.
+  api.export('slidr', 'client');
 
 });
