2e9c281d490db3a825d72379921bbd75b79e153d
Kristoffer Klintberg <krstffr@gmail.com>
2015-02-06 09:53:09 +0100

    Removed dep on reactive var. Fixes #7

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: "krstffr:slidr",
   summary: "A simple slideshow (with support for multiple views).",
-	version: "1.0.3",
+	version: "1.0.4",
 	git: "https://github.com/krstffr/meteor-slidr.git"
 });
 
@@ -9,7 +9,7 @@ Package.onUse(function (api) {
 
 	api.versionsFrom("METEOR@0.9.0");
 
-	api.use(['jquery', 'underscore', 'reactive-var@1.0.4'], 'client');
+	api.use(['jquery', 'underscore'], 'client');
 
   api.addFiles('slidr.js', 'client');
 
