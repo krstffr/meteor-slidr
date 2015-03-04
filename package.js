8b919f3b061120a477155c9624b3545fbd0b068f
Kristoffer Klintberg <krstffr@gmail.com>
2015-03-04 16:44:00 +0100

    Fixed bug where slideshow would be one pixel off to the left.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: "krstffr:slidr",
   summary: "A simple slideshow (with support for multiple views).",
-	version: "1.0.9",
+	version: "1.0.10",
 	git: "https://github.com/krstffr/meteor-slidr.git"
 });
 
